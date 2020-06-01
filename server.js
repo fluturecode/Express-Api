bc1ed77b1bb9a4a917df1ebda76b50da58ed175f
Elizabeth Eidelson <fluturecode@gmail.com>
2020-05-31 21:36:27 -0400

    add cors

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const pingRoute = require("./routes/ping");
 const chuckRoute = require("./routes/chuck");
 const mathRoutes = require("./routes/math");
+const cors = require("cors");
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/", pingRoute);
 app.use("/", chuckRoute);
